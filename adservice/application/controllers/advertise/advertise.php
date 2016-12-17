b5dd876acf649a236529b5432d8c243f79056e9e
yuecaiming <616443378@qq.com>
2016-12-17 19:05:52 +0800

    fix

--- adservice/application/controllers/advertise/advertise.php
+++ adservice/application/controllers/advertise/advertise.php
@@ -21,11 +21,11 @@ class Advertise extends MY_Controller {
         );
 
         $column = array(
-            'id' => 'id',
+            '__checkbox__' => 'id',
             '作品名称' => 'productname|show',
             '作者名称' => 'cb_name|show',
             '指导老师' => 'teachername|show',
-            '{sort|updatetime}更新时间' => 'updatetime|show',
+            '更新时间' => 'updatetime|show',
         );
 
         $this->_setConfig(array(
@@ -56,10 +56,48 @@ class Advertise extends MY_Controller {
 
     }
 
+    public function export(){
+        param_get(
+            array(
+                'id' => 'STRING',
+            ),'', $params, array()
+        );
+        $ids = $params['_GET']['id'];
+        $m = F::$f->Model_Advertiser;
+        $DATA = $m->select(array('id' => explode(',',$ids)),array('select'=>'id'));
+        $title = array('ID');
+        Export::exportExcel($title,$DATA,'作品列表','advertise.xlsx');
+    }
+
 }
 class MyScoffoldHelper extends CommonScaffoldHelper {
     public function cb_name($item){
         return d($item['nickname'],$item['realname'],'--');
+    }
+    public function beforeListTableFootRender(){
+        $html = <<<HTML
+    <tr class="dark">
+        <td colspan="100">
+            <input type="checkbox" class="sel-all"/>
+            <a class='label label label-info' onclick="exports();">导出</a>
+        </td>
+    </tr>
+<script language="JavaScript" type="application/javascript">
+        function exports(){
+            var chk_value =[]; 
+            $('input[class="sel-item"]:checked').each(function(){ 
+            chk_value.push($(this).val()); 
+            }); 
+            if(chk_value == ''){
+                alert('请选择需要导出的数据！');
+                return false;
+            }
+           location.href = "/advertise/advertise/export?id="+chk_value;//location.href实现客户端页面的跳转
+           
+        }
+</script>
+HTML;
+        echo $html;
     }
 
 
